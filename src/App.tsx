c89585c99b9c407d02bd713915be25000806955d
CArCRT <ismealkamara20@gmail.com>
2025-12-19 01:06:25 +0000

    perf: enable code-splitting with React.lazy and Suspense for all routes

--- src/App.tsx
+++ src/App.tsx
@@ -14,30 +14,32 @@ function ScrollToTop() {
 }
 
 // Pages
-import Index from "./pages/Index";
-import About from "./pages/About";
-import MissionVision from "./pages/MissionVision";
-import History from "./pages/History";
-import Leadership from "./pages/Leadership";
-import Programs from "./pages/Programs";
-import ImpactStories from "./pages/ImpactStories";
-import News from "./pages/News";
-import NewsDetail from "./pages/NewsDetail";
-import Events from "./pages/Events";
-import EventDetail from "./pages/EventDetail";
-import GetInvolved from "./pages/GetInvolved";
-import Partners from "./pages/Partners";
-import CoachingPartnerDetail from "./pages/CoachingPartnerDetail";
-import Donate from "./pages/Donate";
-import Contact from "./pages/Contact";
-import Resources from "./pages/Resources";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import TermsOfUse from "./pages/TermsOfUse";
-import Accessibility from "./pages/Accessibility";
-// Admin pages removed
-import RestoringAgriSolutionEnterprises from "./pages/RestoringAgriSolutionEnterprises";
+
+import React, { Suspense } from "react";
+const Index = React.lazy(() => import("./pages/Index"));
+const About = React.lazy(() => import("./pages/About"));
+const MissionVision = React.lazy(() => import("./pages/MissionVision"));
+const History = React.lazy(() => import("./pages/History"));
+const Leadership = React.lazy(() => import("./pages/Leadership"));
+const Programs = React.lazy(() => import("./pages/Programs"));
+const ImpactStories = React.lazy(() => import("./pages/ImpactStories"));
+const News = React.lazy(() => import("./pages/News"));
+const NewsDetail = React.lazy(() => import("./pages/NewsDetail"));
+const Events = React.lazy(() => import("./pages/Events"));
+const EventDetail = React.lazy(() => import("./pages/EventDetail"));
+const GetInvolved = React.lazy(() => import("./pages/GetInvolved"));
+const Partners = React.lazy(() => import("./pages/Partners"));
+const CoachingPartnerDetail = React.lazy(() => import("./pages/CoachingPartnerDetail"));
+const Donate = React.lazy(() => import("./pages/Donate"));
+const Contact = React.lazy(() => import("./pages/Contact"));
+const Resources = React.lazy(() => import("./pages/Resources"));
+const PrivacyPolicy = React.lazy(() => import("./pages/PrivacyPolicy"));
+const TermsOfUse = React.lazy(() => import("./pages/TermsOfUse"));
+const Accessibility = React.lazy(() => import("./pages/Accessibility"));
+const RestoringAgriSolutionEnterprises = React.lazy(() => import("./pages/RestoringAgriSolutionEnterprises"));
 
 const queryClient = new QueryClient();
+
 
 
 const App = () => (
@@ -47,30 +49,32 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/mission-vision" element={<MissionVision />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/leadership" element={<Leadership />} />
-          <Route path="/programs" element={<Programs />} />
-          <Route path="/impact-stories" element={<ImpactStories />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/news/:id" element={<NewsDetail />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/events/:id" element={<EventDetail />} />
-          <Route path="/get-involved" element={<GetInvolved />} />
-          <Route path="/partners" element={<Partners />} />
-          <Route path="/coaching-partners/:slug" element={<CoachingPartnerDetail />} />
-          <Route path="/donate" element={<Donate />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-of-use" element={<TermsOfUse />} />
-          <Route path="/accessibility" element={<Accessibility />} />
-          {/* Admin routes removed */}
-          <Route path="/partners/restoring-agrisolution-enterprises" element={<RestoringAgriSolutionEnterprises />} />
-        </Routes>
+        <Suspense fallback={<div className="py-20 text-center text-lg">Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/mission-vision" element={<MissionVision />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/leadership" element={<Leadership />} />
+            <Route path="/programs" element={<Programs />} />
+            <Route path="/impact-stories" element={<ImpactStories />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/news/:id" element={<NewsDetail />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/events/:id" element={<EventDetail />} />
+            <Route path="/get-involved" element={<GetInvolved />} />
+            <Route path="/partners" element={<Partners />} />
+            <Route path="/coaching-partners/:slug" element={<CoachingPartnerDetail />} />
+            <Route path="/donate" element={<Donate />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-use" element={<TermsOfUse />} />
+            <Route path="/accessibility" element={<Accessibility />} />
+            {/* Admin routes removed */}
+            <Route path="/partners/restoring-agrisolution-enterprises" element={<RestoringAgriSolutionEnterprises />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
